5dd599bd6dd8f3db2d463c3d99fd57148b03069b
SHREYA <shreyaarush2015@gmail.com>
2025-10-03 22:52:49 +0530

    Module_02 "Logical and bitwise operator" questions

---
+++ Module_02/Logical and bitwise operator/ques03.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+
+    scanf("%d%d",&a,&b);
+
+    if(a!=b)
+
+        printf("0\n");
+
+    else if(a==b)
+
+        printf("\n");
+
+    if(a!=b)
+
+    printf("1\n");
+
+    else if(a==b)
+
+printf("0\n");
+
+return 0;
+
+}
---
+++ Module_02/Logical and bitwise operator/ques10.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+int a;
+
+scanf("%d%d",&a);
+
+printf("%d",a>>2);
+
+return 0;
+
+}
